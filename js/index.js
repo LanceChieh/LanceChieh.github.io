b1a08651d70c1d0ce4225a7b2c55ed2ac178bce2
Lance Chang <9thflr.changchinchieh@gmail.com>
2017-07-21 20:48:38 +0800

    tiniline.ver01

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 
     /*================FAQ=============*/
     $("#testQuestion, #testQuestion2,#testQuestion3,#testQuestion4,#testQuestion5,#testQuestion6,#testQuestion7,#testQuestion8,#testQuestion9,#testQuestion10,#testQuestion11,#testQuestion12,#testQuestion13,#testQuestion14,#testQuestion15").click(function(){
-      $(this).find('i').toggleClass('fa fa-plus').css("color","#33ccff");
-      $(this).find('i').toggleClass('fa fa-minus').css("color","#33ccff");
+      $(this).find('i').toggleClass('fa fa-plus').css("color","#00c300");
+      $(this).find('i').toggleClass('fa fa-minus').css("color","#00c300");
     });
 });
